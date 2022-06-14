e99d76879d2493b91803cec02aa17f005957c188
Puja Banerjee <pujab1160@gmail.com>
2022-06-14 19:41:38 +0530

    initial file structure

--- src/blogpost.js
+++ src/blogpost.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import './blogpost.css';
 
 const Blog =()=>{
-
-    const [data, setData] = useState("");
+    const [name, setName] = useState("");
+    const [display,setDisplay] = useState("");
+    const [ blog, setBlog] = useState([]);
+    const submit=()=>{
+        let prevblog = [...blog]
+        prevblog.push({name:name , body : display});
+        setBlog(prevblog);
+        setName("");
+        setDisplay("");
+        console.log(prevblog);
+    }
     return(
         <div className='container'>
             <div className='post'>
                 <input className='first' type="text" placeholder="Name..."
-                value={data}
+                value={name} onChange={(e)=>setName(e.target.value)}/>
                 
-                <input className='description' type="text" placeholder="Write your comment..."/>
-                <button className='add'>Submit</button>
+                <input className='description' type="text" placeholder="Write your comment..."
+                 value={display} onChange={(e)=>setDisplay(e.target.value)}/>
+
+                <button onClick={submit} className='add'>Submit</button>
             </div>
             <div className='display'>
                 <div className='eachblog'>
-                    <p></p>
-                    <button className='dlt'>Remove</button>
+                    {blog.map((item)=>{return(<div>
+                        <p>{item.name}</p>
+                        <p>{item.body}</p>
+                </div>)})} 
+                    
                 </div>
             </div>
         </div>
